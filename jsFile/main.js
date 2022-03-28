454261b3ac15e2ec7d49aeb361c0ac9c89b4d012
go-bi <77657159+go-bi@users.noreply.github.com>
2022-03-28 15:14:50 +0800

    Update main.js
    添加des-ede3-cbc

--- jsFile/main.js
+++ jsFile/main.js
@@ -7,7 +7,7 @@ function burpJsEncrypter(rawPayload){
 	//===============加密开始=================
 
 	encryptedPayload = hex_md5(rawPayload);
-
+	// encryptedPayload = C_Des3.encrypt(rawPayload);
 	//===============加密结束=================
 
 	return encryptedPayload;
